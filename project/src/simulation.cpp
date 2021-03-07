241aa407cdaf19a31d3e00f8289b3e8a2d9467bd
PotatoAim101 <juliette.rossie@gmail.com>
2021-03-07 10:39:29 +0100

    reorganized project

---
+++ project/src/simulation.cpp
@@ -0,0 +1,5 @@
+//
+// Created by roundedglint585 on 3/3/21.
+//
+
+#include "simulation.h"
---
+++ project/src/simulation.h
@@ -0,0 +1,23 @@
+//
+// Created by roundedglint585 on 3/3/21.
+//
+
+#ifndef PROJECT_SIMULATION_H
+#define PROJECT_SIMULATION_H
+#include "vcl/vcl.hpp"
+
+// SPH Particle
+struct particle_element
+{
+    vcl::vec3 p; // Position
+    vcl::vec3 v; // Speed
+    vcl::vec3 f; // Force
+
+    float rho;      // density at this particle position
+    float pressure; // pressure at this particle position
+
+    particle_element() : p{0,0,0},v{0,0,0},f{0,0,0},rho(0),pressure(0) {}
+};
+
+
+#endif //PROJECT_SIMULATION_H
